4fb12844d469b4724a7443134e21e0601db8ebec
Abhinav Sawarkar <abhisawarkar85@gmail.com>
2021-11-21 14:53:00 +0530

    Added Fade animation to Property Item

--- src/components/Properties/PropertyItem.js
+++ src/components/Properties/PropertyItem.js
@@ -1,27 +1,32 @@
 import React from "react";
 import StarIcon from "@mui/icons-material/Star";
 import classes from "./PropertyItem.module.css";
+import Fade from "react-reveal/Fade";
 
 const PropertyItem = ({ src, title, superhost, type, rating, beds }) => {
   return (
-    <li className={classes.propItem}>
-      <div className={classes.propImg}>
-        <img src={src} alt={title} />
-      </div>
-      <div className={classes.propSpecs}>
-        {superhost && <span className={classes.propSuperHost}>SUPERHOST</span>}
-        <span className={classes.propInfo}>
-          <span className={classes.propType}>
-            {type}. {beds ? `${beds} beds` : ""}
+    <Fade bottom>
+      <li className={classes.propItem}>
+        <div className={classes.propImg}>
+          <img src={src} alt={title} loading="lazy" />
+        </div>
+        <div className={classes.propSpecs}>
+          {superhost && (
+            <span className={classes.propSuperHost}>SUPERHOST</span>
+          )}
+          <span className={classes.propInfo}>
+            <span className={classes.propType}>
+              {type}. {beds ? `${beds} beds` : ""}
+            </span>
+            <span className={classes.propRating}>
+              <StarIcon />
+              <span>{rating}</span>
+            </span>
           </span>
-          <span className={classes.propRating}>
-            <StarIcon />
-            <span>{rating}</span>
-          </span>
-        </span>
-      </div>
-      <p className={classes.propTitle}>{title}</p>
-    </li>
+        </div>
+        <p className={classes.propTitle}>{title}</p>
+      </li>
+    </Fade>
   );
 };
 
